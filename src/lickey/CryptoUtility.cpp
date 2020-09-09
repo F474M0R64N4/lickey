c7243cfb8130c87c676d3d719e34797df34b5be1
8345738475 <32421222@googlepriv.com>
2020-09-09 22:44:14 +0300

    Update CryptoUtility.cpp

--- src/lickey/CryptoUtility.cpp
+++ src/lickey/CryptoUtility.cpp
@@ -10,48 +10,48 @@ using namespace std;
 
 namespace lickey
 {
-	bool Encrypt(const std::string& data, const size_t datalen, std::string key, std::string iv, std::string& dest,
-	             size_t& destlen)
+	auto Encrypt(const std::string& data, const size_t datalen, std::string key, std::string iv, std::string& dest,
+	             size_t& destlen) -> bool
 	{
 		const auto ciphered_buffer = cipher::encrypt(cipher_t::aes_256_cbc, padding_t::pkcs7, iv, key.append(key), data);
 		dest = ciphered_buffer;
 		return true;
 	}
 
-	bool Decrypt(const std::string& data, const size_t datalen, std::string key, std::string iv, std::string& dest,
-	             size_t& destlen)
+	auto Decrypt(const std::string& data, const size_t datalen, std::string key, std::string iv, std::string& dest,
+	             size_t& destlen) -> bool
 	{
 		const auto decrypted_buffer = cipher::decrypt(cipher_t::aes_256_cbc, padding_t::pkcs7, iv, key.append(key), data);
 		dest = decrypted_buffer;
 		return true;
 	}
 
-	bool MD5(std::string data, size_t datalen, std::string& hash)
+	auto MD5(std::string data, size_t datalen, std::string& hash) -> bool
 	{
 		hash = make_hash(hash_t::md5, data);
 		datalen = data.length();
 		return true;
 	}
 
-	bool SHA256(std::string& data, size_t datalen, std::string& hash)
+	auto SHA256(std::string& data, size_t datalen, std::string& hash) -> bool
 	{
 		hash = make_hash(hash_t::sha256, data);
 		return false;
 	}
 
-	void EncodeBase64(std::string& data, std::string& str)
+	auto EncodeBase64(std::string& data, std::string& str) -> void
 	{
 		str = to_base64(data);
 	}
 
-	void DecodeBase64(std::string& str, std::string& data, int& datalen)
+	auto DecodeBase64(std::string& str, std::string& data, int& datalen) -> void
 	{
 		data = base64::decode(str);
 		datalen = str.length();
 	}
 
 
-	bool MakeSalt(Salt& salt)
+	auto MakeSalt(Salt& salt) -> bool
 	{
 		std::string tmp;
 		std::string encoded;
