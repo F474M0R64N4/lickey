8ca51b07fe4fa58cdf24a9a301f40e36140c89f0
8345738475 <32421222@googlepriv.com>
2020-09-09 23:44:08 +0300

    Update lickey_standalone.cpp

--- src/lickey_standalone/lickey_standalone.cpp
+++ src/lickey_standalone/lickey_standalone.cpp
@@ -11,17 +11,63 @@ int main()
 
 	lickey::LicenseManager license_manager("v", "a");
 	lickey::License license;
-	license_manager.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\werds(84-16-F9-F5-79-9F))", keys.front(), license);
+	license_manager.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\putin(84-16-F9-F5-79-9F))",
+	                     keys.front(), license);
 
 	if (license.FeatureMap().IsExpired("full") == false)
 	{
-		std::cout << "the license is not expired\n";
+		std::cout << "full:: the license is not expired\n";
 	}
-	
+	else
+	{
+		std::cout << "full:: the license is expired\n";
+	}
+
 	if (license.FeatureMap().IsValid("full") == true)
 	{
-		std::cout << "license is valid\n";
+		std::cout << "full:: license is valid\n";
 	}
+	else
+	{
+		std::cout << "full:: license is not valid\n";
+	}
+	// trial
+	if (license.FeatureMap().IsExpired("trial") == false)
+	{
+		std::cout << "trial:: the license is not expired\n";
+	}
+	else
+	{
+		std::cout << "trial:: the license is expired\n";
+	}
+
+	if (license.FeatureMap().IsValid("trial") == true)
+	{
+		std::cout << "trial:: license is valid\n";
+	}
+	else
+	{
+		std::cout << "trial:: license is not valid\n";
+	}
+	// 2days
+	if (license.FeatureMap().IsExpired("2days") == false)
+	{
+		std::cout << "2days:: the license is not expired\n";
+	}
+	else
+	{
+		std::cout << "2days:: the license is expired\n";
+	}
+
+	if (license.FeatureMap().IsValid("full") == true)
+	{
+		std::cout << "2days:: license is valid\n";
+	}
+	else
+	{
+		std::cout << "2days:: license is not valid\n";
+	}
+
 
 	getchar();
 }
