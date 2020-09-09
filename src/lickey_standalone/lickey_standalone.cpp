661f55b769fb89da3485e54d66a29731449e0736
8345738475 <32421222@googlepriv.com>
2020-09-09 19:39:52 +0300

    update code

--- src/lickey_standalone/lickey_standalone.cpp
+++ src/lickey_standalone/lickey_standalone.cpp
@@ -12,12 +12,20 @@ int main()
 
 	if (keys.empty())
 	{
-		return;
+		//return 0;
 	}
 
 	lickey::LicenseManager licMgr("v", "a");
 	lickey::License lic;
 	licMgr.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\werds(84-16-F9-F5-79-9F))", keys.front(), lic);
 
-	std::cout << "Hello World!\n";
+	if (lic.FeatureMap().IsExpired("full") == false)
+	{
+		std::cout << "the license is not expired\n";
+	}
+	
+	if (lic.FeatureMap().IsValid("full") == true)
+	{
+		std::cout << "license is valid\n";
+	}
 }
