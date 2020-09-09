358cf0a7f42a5b4538f84d3880e8513437b3434a
8345738475 <32421222@googlepriv.com>
2020-09-09 18:56:33 +0300

    nvm

--- src/lickey_test/HardwareKeyTest.h
+++ src/lickey_test/HardwareKeyTest.h
@@ -19,11 +19,7 @@ BOOST_AUTO_TEST_CASE(Constructor01)
     BOOST_CHECK_EQUAL(5, keys.size());
     if(5 == keys.size())
     {
-        BOOST_CHECK_EQUAL("CC-E1-D5-41-21-D6", keys[0].Value());
-        BOOST_CHECK_EQUAL("80-FA-5B-11-34-19", keys[1].Value());
-        BOOST_CHECK_EQUAL("D0-53-49-83-7B-4C", keys[2].Value());
-        BOOST_CHECK_EQUAL("D0-53-49-83-8C-7C", keys[3].Value());
-        BOOST_CHECK_EQUAL("0A-00-27-00-00-15", keys[4].Value());
+        BOOST_CHECK_EQUAL("84-16-F9-F5-79-9F", keys[0].Value());
     }
 }
 
--- src/lickey_test/LicenseManagerTest.h
+++ src/lickey_test/LicenseManagerTest.h
@@ -9,22 +9,21 @@
 
 BOOST_AUTO_TEST_SUITE(LicenseManagerTest)
 
-BOOST_AUTO_TEST_CASE(Constructor01)
-{
-    lickey::HardwareKeyGetter keyGetter;
-    lickey::HardwareKeys keys = keyGetter();
-    if(keys.empty())
-    {
-        return;
-    }
-    {
-        lickey::LicenseManager licMgr("v", "a");
-        lickey::License lic;
-        licMgr.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\1(84-16-F9-F5-79-9F))", keys.front(), lic);
+BOOST_AUTO_TEST_CASE(Constructor01) {
+  lickey::HardwareKeyGetter keyGetter;
+  lickey::HardwareKeys keys = keyGetter();
 
-        BOOST_CHECK_EQUAL(false, lic.FeatureMap().IsExpired("full"));
-        BOOST_CHECK_EQUAL(true, lic.FeatureMap().IsValid("full"));
-    }
+  if (keys.empty()) {
+    return;
+  }
+
+  {
+    lickey::LicenseManager licMgr("v", "a");
+    lickey::License lic;
+    licMgr.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\werds(84-16-F9-F5-79-9F))", keys.front(), lic);
+    BOOST_CHECK_EQUAL(false, lic.FeatureMap().IsExpired("full"));
+    BOOST_CHECK_EQUAL(true, lic.FeatureMap().IsValid("full"));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
