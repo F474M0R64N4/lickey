6f7b1a8c7cf4e5fd990bb4147ccfd93cc211a18a
8345738475 <32421222@googlepriv.com>
2020-09-12 04:41:37 +0300

    Update lickey_standalone.cpp

--- src/lickey_standalone/lickey_standalone.cpp
+++ src/lickey_standalone/lickey_standalone.cpp
@@ -11,7 +11,7 @@ int main()
 
 	lickey::LicenseManager license_manager("v", "a");
 	lickey::License license;
-	license_manager.Load(R"(C:\Users\WORK\Desktop\lickey\src\lickey_gen\x64\Debug\vl(8613cff15aca54d4b41de733b957c9b84377c4cbe95d63f9e5dc3540cdabbce0))",
+	license_manager.Load(R"(license)",
 	                     keys.front(), license);
 
 	if (license.FeatureMap().IsExpired("full") == false)
@@ -31,42 +31,42 @@ int main()
 	{
 		std::cout << "full:: license is not valid\n";
 	}
-	// trial
-	if (license.FeatureMap().IsExpired("trial") == false)
-	{
-		std::cout << "trial:: the license is not expired\n";
-	}
-	else
-	{
-		std::cout << "trial:: the license is expired\n";
-	}
+	//// trial
+	//if (license.FeatureMap().IsExpired("trial") == false)
+	//{
+	//	std::cout << "trial:: the license is not expired\n";
+	//}
+	//else
+	//{
+	//	std::cout << "trial:: the license is expired\n";
+	//}
 
-	if (license.FeatureMap().IsValid("trial") == true)
-	{
-		std::cout << "trial:: license is valid\n";
-	}
-	else
-	{
-		std::cout << "trial:: license is not valid\n";
-	}
-	// 2days
-	if (license.FeatureMap().IsExpired("2days") == false)
-	{
-		std::cout << "2days:: the license is not expired\n";
-	}
-	else
-	{
-		std::cout << "2days:: the license is expired\n";
-	}
+	//if (license.FeatureMap().IsValid("trial") == true)
+	//{
+	//	std::cout << "trial:: license is valid\n";
+	//}
+	//else
+	//{
+	//	std::cout << "trial:: license is not valid\n";
+	//}
+	//// 2days
+	//if (license.FeatureMap().IsExpired("2days") == false)
+	//{
+	//	std::cout << "2days:: the license is not expired\n";
+	//}
+	//else
+	//{
+	//	std::cout << "2days:: the license is expired\n";
+	//}
 
-	if (license.FeatureMap().IsValid("full") == true)
-	{
-		std::cout << "2days:: license is valid\n";
-	}
-	else
-	{
-		std::cout << "2days:: license is not valid\n";
-	}
+	//if (license.FeatureMap().IsValid("full") == true)
+	//{
+	//	std::cout << "2days:: license is valid\n";
+	//}
+	//else
+	//{
+	//	std::cout << "2days:: license is not valid\n";
+	//}
 
 
 	getchar();
