69c32df7610b9f722827d1fd6da8e5f94c8ecd54
8345738475 <32421222@googlepriv.com>
2020-09-12 17:54:47 +0300

    comment useless

--- src/lickey/FileUtility.cpp
+++ src/lickey/FileUtility.cpp
@@ -6,29 +6,29 @@
 
 namespace lickey
 {
-	auto get_extension(const std::string& filePath) -> std::string
-	{
-		const auto pos = filePath.find_last_of('.');
+//	auto get_extension(const std::string& filePath) -> std::string
+//	{
+//		const auto pos = filePath.find_last_of('.');
+//
+//		if (std::string::npos == pos)
+//		{
+//			return "";
+//		}
+//
+//		return filePath.substr(pos, filePath.size() - pos);
+//	}
 
-		if (std::string::npos == pos)
-		{
-			return "";
-		}
-
-		return filePath.substr(pos, filePath.size() - pos);
-	}
-
-	auto get_base_file_path(const std::string& filePath) -> std::string
-	{
-		const auto pos = filePath.find_last_of('.');
-
-		if (std::string::npos == pos)
-		{
-			return filePath;
-		}
-
-		return filePath.substr(0, pos);
-	}
+//	auto get_base_file_path(const std::string& filePath) -> std::string
+//	{
+//		const auto pos = filePath.find_last_of('.');
+//
+//		if (std::string::npos == pos)
+//		{
+//			return filePath;
+//		}
+//
+//		return filePath.substr(0, pos);
+//	}
 
 	auto get_folder_path(const std::string& filePath) -> std::string
 	{
@@ -42,17 +42,17 @@ namespace lickey
 		return filePath.substr(0, pos);
 	}
 
-	auto get_filename(const std::string& filePath) -> std::string
-	{
-		const auto pos = filePath.find_last_of('\\');
-
-		if (std::string::npos == pos)
-		{
-			return filePath;
-		}
-
-		return filePath.substr(pos + 1, filePath.size() - pos - 1);
-	}
+//	auto get_filename(const std::string& filePath) -> std::string
+//	{
+//		const auto pos = filePath.find_last_of('\\');
+//
+//		if (std::string::npos == pos)
+//		{
+//			return filePath;
+//		}
+//
+//		return filePath.substr(pos + 1, filePath.size() - pos - 1);
+//	}
 
 	auto get_exe_file_path() -> std::string
 	{
@@ -63,46 +63,46 @@ namespace lickey
 		return path;
 	}
 
-	auto get_exe_folder_path() -> std::string
-	{
-		return get_folder_path(get_exe_file_path());
-	}
+//	auto get_exe_folder_path() -> std::string
+//	{
+//		return get_folder_path(get_exe_file_path());
+//	}
 
-	auto give_postfix(const std::string& filepath, const std::string& postfix) -> std::string
-	{
-		const auto pos = filepath.find_last_of('.');
-		auto ans = std::string::npos == pos
-			           ? filepath + "_" + postfix
-			           : filepath.substr(0, pos) + "_" + postfix + filepath.substr(pos, filepath.size() - pos);
-		return ans;
-	}
+//	auto give_postfix(const std::string& filepath, const std::string& postfix) -> std::string
+//	{
+//		const auto pos = filepath.find_last_of('.');
+//		auto ans = std::string::npos == pos
+//			           ? filepath + "_" + postfix
+//			           : filepath.substr(0, pos) + "_" + postfix + filepath.substr(pos, filepath.size() - pos);
+//		return ans;
+//	}
 
-	auto change_extension(const std::string& filepath, const std::string& new_ext) -> std::string
-	{
-		const auto pos = filepath.find_last_of('.');
+//	auto change_extension(const std::string& filepath, const std::string& new_ext) -> std::string
+//	{
+//		const auto pos = filepath.find_last_of('.');
+//
+//		if (std::string::npos == pos)
+//		{
+//			return filepath + "." + new_ext;
+//		}
+//
+//		return filepath.substr(0, pos) + "." + new_ext;
+//	}
 
-		if (std::string::npos == pos)
-		{
-			return filepath + "." + new_ext;
-		}
-
-		return filepath.substr(0, pos) + "." + new_ext;
-	}
-
-	auto join_path(const std::string& folderpath, const std::string& filepath) -> std::string
-	{
-		if (folderpath.empty())
-		{
-			return filepath;
-		}
-
-		if ('\\' == folderpath.back())
-		{
-			return folderpath + filepath;
-		}
-
-		return folderpath + "\\" + filepath;
-	}
+//	auto join_path(const std::string& folderpath, const std::string& filepath) -> std::string
+//	{
+//		if (folderpath.empty())
+//		{
+//			return filepath;
+//		}
+//
+//		if ('\\' == folderpath.back())
+//		{
+//			return folderpath + filepath;
+//		}
+//
+//		return folderpath + "\\" + filepath;
+//	}
 
 	auto read_lines(const std::string& filepath, std::vector<std::string>& lines) -> bool
 	{
--- src/lickey/FileUtility.h
+++ src/lickey/FileUtility.h
@@ -3,16 +3,15 @@
 
 namespace lickey
 {
-	auto get_extension(const std::string& filepath) -> std::string;
-	auto get_base_file_path(const std::string& filepath) -> std::string;
-	auto get_filename(const std::string& filepath) -> std::string;
+	//auto get_extension(const std::string& filepath) -> std::string;
+	//auto get_base_file_path(const std::string& filepath) -> std::string;
+	//auto get_filename(const std::string& filepath) -> std::string;
 	auto get_folder_path(const std::string& filepath) -> std::string;
 	auto get_exe_file_path() -> std::string;
-	auto get_exe_folder_path() -> std::string;
-	auto give_postfix(const std::string& filepath, const std::string& postfix) -> std::string;
-	auto change_extension(const std::string& filepath, const std::string& new_ext) -> std::string;
-	///< newExt must be without "."
-	auto join_path(const std::string& folderpath, const std::string& filepath) -> std::string;
+	//auto get_exe_folder_path() -> std::string;
+	//auto give_postfix(const std::string& filepath, const std::string& postfix) -> std::string;
+	//auto change_extension(const std::string& filepath, const std::string& new_ext) -> std::string;
+	//auto join_path(const std::string& folderpath, const std::string& filepath) -> std::string;
 	auto read_lines(const std::string& filepath, std::vector<std::string>& lines) -> bool;
 }
 #endif // FILEUTILITY_H
